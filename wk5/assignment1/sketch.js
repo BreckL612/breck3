5c103b6c7834733a8218043ac1cf5f4d55dbf39d
BreckL612 <81171964+BreckL612@users.noreply.github.com>
2021-04-24 17:36:35 -0400

    Add wk5 assignment 1

---
+++ wk5/assignment1/sketch.js
@@ -0,0 +1,55 @@
+function setup() {
+  createCanvas(800, 600);
+  background ( random(400), random(255), random(255) );
+  
+ 
+    fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+  fill(random(440), random(200), random(400));
+  square(random(800), random(600), 10);
+
+//I know there is a way to not have to write all of this, but I couldn't remmeber how to do it or what it was called
+
+  
+}function draw() {
+  }
