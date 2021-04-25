06a24ea33a6d47946b07d300c964182bad1756a9
BreckL612 <81171964+BreckL612@users.noreply.github.com>
2021-04-24 20:02:20 -0400

    stuff

---
+++ wk5/assignment4/sketch.js
@@ -0,0 +1,122 @@
+function setup() {
+  createCanvas(400, 200);
+  background ( random(400), random(255), random(255) );
+  
+ 
+    fill(random(440), random(200), random(400));
+  square((0), (0), 40);
+  fill(random(440), random(200), random(400));
+  square((40), (40), 40);
+  fill(random(440), random(200), random(400));
+  square((80), (80), 40);
+  fill(random(440), random(200), random(400));
+  square((120), (120), 40);
+  fill(random(440), random(200), random(400));
+  square((160), (160), 40);
+  
+  fill(random(440), random(200), random(400));
+  square((40), random(0), 40);
+  fill(random(440), random(200), random(400));
+  square((80), (40), (40));
+  fill(random(440), random(200), random(400));
+  square((120), (80), (40));
+  fill(random(440), random(200), random(400));
+  square((160), (120), (40));
+  fill(random(440), random(200), random(400));
+   square((200), (120), (40));
+  fill(random(440), random(200), random(400));
+   square((240), (160), (40));
+  fill(random(440), random(200), random(400));
+   square((280), (200), (40));
+  fill(random(440), random(200), random(400));
+   square((0), (40), (40));
+  fill(random(440), random(200), random(400));
+   square((0), (80), (40));
+   fill(random(440), random(200), random(400));
+   square((0), (120), (40));
+   fill(random(440), random(200), random(400));
+   square((0), (160), (40));
+   fill(random(440), random(200), random(400));
+   square((40), (80), (40));
+  fill(random(440), random(200), random(400));
+   square((40), (120), (40));
+  fill(random(440), random(200), random(400));
+   square((40), (160), (40));
+  fill(random(440), random(200), random(400));
+   square((80), (160), (40));
+  fill(random(440), random(200), random(400));
+   square((80), (0), (40));
+  
+  fill(random(440), random(200), random(400));
+   square((120), (0), (40));
+  fill(random(440), random(200), random(400));
+   square((120), (40), (40));
+  
+    fill(random(440), random(200), random(400));
+   square((160), (0), (40));
+  fill(random(440), random(200), random(400));
+   square((160), (40), (40));
+  fill(random(440), random(200), random(400));
+   square((160), (80), (40));
+  
+  fill(random(440), random(200), random(400));
+   square((200), (0), (40));
+  fill(random(440), random(200), random(400));
+   square((200), (40), (40));
+  fill(random(440), random(200), random(400));
+   square((200), (80), (40));
+  
+  
+    fill(random(440), random(200), random(400));
+   square((240), (0), (40));
+  fill(random(440), random(200), random(400));
+   square((240), (40), (40));
+    fill(random(440), random(200), random(400));
+   square((240), (80), (40));
+    fill(random(440), random(200), random(400));
+   square((240), (120), (40));
+    fill(random(440), random(200), random(400));
+   square((240), (160), (40));
+  
+  fill(random(440), random(200), random(400));
+   square((280), (0), (40));
+  fill(random(440), random(200), random(400));
+   square((280), (40), (40));
+    fill(random(440), random(200), random(400));
+   square((280), (80), (40));
+    fill(random(440), random(200), random(400));
+   square((280), (120), (40));
+    fill(random(440), random(200), random(400));
+   square((280), (160), (40));
+  
+  fill(random(440), random(200), random(400));
+   square((320), (0), (40));
+  fill(random(440), random(200), random(400));
+   square((320), (40), (40));
+    fill(random(440), random(200), random(400));
+   square((320), (80), (40));
+    fill(random(440), random(200), random(400));
+   square((320), (120), (40));
+    fill(random(440), random(200), random(400));
+   square((320), (160), (40));
+  
+  fill(random(440), random(200), random(400));
+   square((360), (0), (40));
+  fill(random(440), random(200), random(400));
+   square((360), (40), (40));
+    fill(random(440), random(200), random(400));
+   square((360), (80), (40));
+    fill(random(440), random(200), random(400));
+   square((360), (120), (40));
+    fill(random(440), random(200), random(400));
+   square((360), (160), (40));
+  
+  
+  
+  
+
+//I know there is a way to not have to write all of this, but I couldn't remmeber how to do it or what it was called
+
+  
+}function draw() {
+  }
